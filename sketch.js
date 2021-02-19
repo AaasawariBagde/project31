6ada1a93dee3125d32a433385d0c65cbe96ec026
Aasawari Bagde <75113161+AaasawariBagde@users.noreply.github.com>
2021-02-19 13:15:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+class Drop {
+    constructor(x , y) {
+     var options = {
+         restiution: 0.1,
+         friction: 0.1,
+         density: 4,
+     };
+     this.body = Bodies.circle(x, y, 8, options );
+     this.radius = 8;
+     World.add(world, this.body);
+    }
+
+    changePosition() {
+        if(this.body.position.y > height + 500) {
+            Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+        }
+    }
+
+    display() {
+      fill("blue");
+      ellipseMode(CENTER);
+      ellipse(
+          this.body.position.x,
+          this.body.position.y,
+          this.radius,
+          this.radius
+      );
+    }
+}
